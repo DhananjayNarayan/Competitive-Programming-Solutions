4da0e6fa39e087b1a9472d99b04c5e296db1bc44
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-02-26 23:23:37 +0530

    Update bparty.cpp

--- Hackerearth/bparty.cpp
+++ Hackerearth/bparty.cpp
@@ -1,19 +1,15 @@
 /* Mr. X's birthday is in next month. This time he is planning to invite N of his friends. He wants to distribute some chocolates to all of his friends after party. He went to a shop to buy a packet of chocolates.
 At chocolate shop, each packet is having different number of chocolates. He wants to buy such a packet which contains number of chocolates, which can be distributed equally among all of his friends.
 Help Mr. X to buy such a packet.
-
 Input:
 First line contains T, number of test cases.
 Each test case contains two integers, N and M. where is N is number of friends and M is number number of chocolates in a packet.
-
 Output:
 In each test case output "Yes" if he can buy that packet and "No" if he can't buy that packet.  */  
 
 #include <iostream>
- 
-using namespace std;
- 
-int main() {
+ using namespace std;
+ int main() {
 	int t;
 	cin>>t;
 	int m,n;
@@ -26,4 +22,3 @@ int main() {
 	    cout<<"No"<<endl;
 	}
 }
- 
