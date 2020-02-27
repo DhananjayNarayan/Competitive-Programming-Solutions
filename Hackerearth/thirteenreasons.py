c95c2f6b239af5b3a31a56f770ba47ede743732a
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-02-28 01:37:57 +0530

    Thirteen Reasons Why : Python
    Raghav is currently watching Netflix. He is feeling thrilled after watching Seasons 1, 2 and 3 of 13 Reasons Why, and is desperately waiting for release of Season 4. But the makers of the show are in no mood to release the next season anytime soon. 
    
    The makers of 13 Reasons Why give Raghav a challenge to solve. If he solves this challenge, then they will give exclusive copy of Season 4 to him.
    
    But Raghav is feeling lazy. Can you help him solve this challenge?
    
    Given 3 integers A, B, C. Do the following steps-
    
    Swap A and B.
    Multiply A by C.
    Add C to B.
    Output new values of A and B.

---
+++ Hackerearth/thirteenreasons.py
@@ -0,0 +1,21 @@
+#Raghav is currently watching Netflix. He is feeling thrilled after watching Seasons 1, 2 and 3 of 13 Reasons Why, and is desperately 
+#waiting for release of Season 4. But the makers of the show are in no mood to release the next season anytime soon. 
+#The makers of 13 Reasons Why give Raghav a challenge to solve. 
+#If he solves this challenge, then they will give exclusive copy of Season 4 to him.
+#But Raghav is feeling lazy. Can you help him solve this challenge?
+#Given 3 integers A, B, C. Do the following steps-
+#Swap A and B.
+#Multiply A by C.
+#Add C to B.
+#Output new values of A and B.
+
+#SAMPLE INPUT:
+#13 5 2
+#SAMPLE OUTPUT:
+#10 15
+
+a,b,c=map(int,input().split())
+a,b=b,a
+x=a*c
+y=c+b
+print(x,y)
