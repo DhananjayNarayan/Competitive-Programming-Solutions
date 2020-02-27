af32b45f404014b1e04078c2db20de5c7fb76dbb
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-02-27 23:16:07 +0530

    Palindromic String C++

--- Hackerearth/palinstr.cpp
+++ Hackerearth/palinstr.cpp
@@ -1,12 +1,11 @@
 #include <iostream> 
 #include <string> 
 #include <algorithm> 
+
 using namespace std;
  
-int main() 
-{ 
- 
- string str; 
+int main() { 
+  string str; 
  getline(cin, str); 
  string ostr = str; 
  reverse(str.begin(), str.end()); 
