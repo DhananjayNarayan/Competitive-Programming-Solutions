cd2533f6a4d0800d2926f8fd9f221105064ff212
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-05-24 23:12:32 +0530

    Update and rename HackerRank/Weird.py to HackerRank/Python/Weird.py

--- HackerRank/Python/Weird.py
+++ HackerRank/Python/Weird.py
@@ -3,7 +3,8 @@
 If n is odd, print Weird
 If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20 , print Weird
-If n is even and greater than 20 , print Not Weird  """
+If n is even and greater than 20 , print Not Weird 
+"""
 
 N = int(input())
 if N%2==0 :
