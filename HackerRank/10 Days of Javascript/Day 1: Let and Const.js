56fa27f9f32f911c949597e16f6e8a92f9d09b67
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-08-19 21:11:29 +0530

    Create Day 1: Let and Const.js

---
+++ HackerRank/10 Days of Javascript/Day 1: Let and Const.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    
+    const pi= Math.PI;
+    let r=readLine();
+    let area= pi*r*r;
+    let perimeter= 2*pi*r;
+    
+    console.log(area);
+    console.log(perimeter);
+
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
